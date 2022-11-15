de3b90abd264da1604f49a1f5320268e3603fc40
Shailesh_Kumar_23994 <shaileshkumar23994@gmail.com>
2022-11-15 13:45:39 +0530

    cors fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -129,3 +131,12 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / "media"
+
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+## or set above as False and uncomment below
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:5000',
+# ]
+
