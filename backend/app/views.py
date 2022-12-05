fca5be336d308cec5f9548ab3be2900c9af57641
Shailesh_Kumar_23994 <shaileshkumar23994@gmail.com>
2022-12-05 09:05:07 +0530

    recommendation update

--- backend/app/views.py
+++ backend/app/views.py
@@ -183,30 +183,48 @@ def store_post(request):
         return JsonResponse(data)
 
 @csrf_exempt
-def recommendations(request):
-    if request.method == 'POST' or request.method == 'GET':
-        # username = request.session.get('username', False)
-        username = request.POST.get('username')
-        userlogged = db.user_logged.find_one({"username":username, "status":1})
-        if userlogged:
-            # friend_list=[]
-            # friends=db.user_friend.find({"sourceId":username}, {"_id":0, "targetId":1}).limit(10)
-            # for friend in friends:
-            #     friend_list.append(friend['targetId'])
-            # friend_of_friend_list=[]
-            # friends_of_friends=db.user_friend.find({"sourceId":{'$in':friend_list}}, {"_id":0, "targetId":1}).limit(10)
-            # for friend in friends_of_friends:
-            #     friend_of_friend_list.append(friend['targetId'])
-            # recommendation_list=[]
-            # recommendations=db.user.find({"username":{'$in':friend_list}, "username":{'$in':friend_of_friend_list}}, {"_id":0, "username":1, "imagePath":1}).limit(10)
-            recommendations=db.user.find({"username":{'$regex':"^{username}.+".format(username=username)}}, {"_id":0, "username":1, "imagePath":1}).limit(10)
-            for recommendation in recommendations:
-                recommendation_list.append(recommendation)
-            return JsonResponse(recommendation_list, safe=False)
+# legacy code
 
-        else:
-            data = {'status': 'error', 'message': 'Not logged in'}
-            return JsonResponse(data)
+# def recommendations(request):
+#     if request.method == 'POST' or request.method == 'GET':
+#         # username = request.session.get('username', False)
+#         username = request.POST.get('username')
+#         userlogged = db.user_logged.find_one({"username":username, "status":1})
+#         if userlogged:
+#             # friend_list=[]
+#             # friends=db.user_friend.find({"sourceId":username}, {"_id":0, "targetId":1}).limit(10)
+#             # for friend in friends:
+#             #     friend_list.append(friend['targetId'])
+#             # friend_of_friend_list=[]
+#             # friends_of_friends=db.user_friend.find({"sourceId":{'$in':friend_list}}, {"_id":0, "targetId":1}).limit(10)
+#             # for friend in friends_of_friends:
+#             #     friend_of_friend_list.append(friend['targetId'])
+#             # recommendation_list=[]
+#             # recommendations=db.user.find({"username":{'$in':friend_list}, "username":{'$in':friend_of_friend_list}}, {"_id":0, "username":1, "imagePath":1}).limit(10)
+#             recommendations=db.user.find({"username":{'$regex':"^{username}.+".format(username=username)}}, {"_id":0, "username":1, "imagePath":1}).limit(10)
+#             for recommendation in recommendations:
+#                 recommendation_list.append(recommendation)
+#             return JsonResponse(recommendation_list, safe=False)
+
+#         else:
+#             data = {'status': 'error', 'message': 'Not logged in'}
+#             return JsonResponse(data)
+#     else:
+#         data = {'status': 'error', 'message': 'Invalid request'}
+#         return JsonResponse(data)
+
+# new code
+def recommendations(request):
+    username = request.POST.get('username', '')
+    if request.method == "POST" or request.method == "GET":
+        # recommendations=db.user.find({"username":{'$regex':"^{username}.+".format(username=username)}}, {"_id":0, "username":1, "imagePath":1}).limit(10)
+        recommendations = db.user.find({}, {"_id":0, "username":1, "imagePath":1}).limit(10)
+        recommendation_list=[]
+        for recommendation in recommendations:
+            if recommendation["username"] == username:
+                continue
+            recommendation_list.append(recommendation)
+        return JsonResponse(recommendation_list, safe=False)
     else:
         data = {'status': 'error', 'message': 'Invalid request'}
         return JsonResponse(data)
