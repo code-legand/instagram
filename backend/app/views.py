8a332454b63650a54bc20dc9e068f0ed96a6a072
Shailesh_Kumar_23994 <shaileshkumar23994@gmail.com>
2022-12-09 15:39:23 +0530

    minor test

--- backend/app/views.py
+++ backend/app/views.py
@@ -793,7 +793,7 @@ def fetch_follow_requests(request):
         username  = request.POST.get('username')
         userlogged = db.user_logged.find_one({"username":username, "status":1})
         if userlogged:
-            follow_requests = db.user_follow.find({"targetId":username, "status":"pending"}, {"_id":0}).sort("createdAt")
+            follow_requests = db.user_follow.find({"targetId":username, "status":"pending"}, {"_id":0}).sort("createdAt", -1)
             data = []
             for follow_request in follow_requests:
                 details = db.user.find_one({"username":follow_request['sourceId']})
